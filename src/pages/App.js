b4370d6f2b95720c458b274621fe5dbbd6c76418
HyuriCunha <hyuri.hgc@hotmail.com>
2024-11-12 23:46:37 -0300

    Finalizado desafio acrescentando o Filter

--- src/pages/App.js
+++ src/pages/App.js
@@ -34,9 +34,7 @@ function App() {
   }
 
   const handleRemoveRepo = (id) => {
-    console.log('Removendo registro', id);
-
-    // utilizar filter.
+    setRepos(repos => repos.filter(repo => repo.id !== id));
   }
 
 
